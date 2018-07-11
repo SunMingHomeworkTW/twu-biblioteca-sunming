4d694d687a220f7ec8a9923a309413da32931f84
SunMing <1229536801@qq.com>
2018-07-11 13:26:23 +0800

    complete show welcome message and the test passed

--- src/com/twu/biblioteca/view/BibliotecaView.java
+++ src/com/twu/biblioteca/view/BibliotecaView.java
@@ -1,4 +1,9 @@
 package com.twu.biblioteca.view;
 
+import static com.twu.biblioteca.view.Hint.*;
+
 public class BibliotecaView {
+    public void showWelcomeMessage() {
+        System.out.println(WECLOME_HINT);
+    }
 }
